90d3fd4b71331831643635004391ba71138a51c4
barismeral <30953081+barismeral@users.noreply.github.com>
2019-11-02 23:43:14 +0300

    upload v2
    Yeni Kanallar eklendi
    Ses Formatı kalite seçeneği eklendi.

--- demo/fenomen.js
+++ demo/fenomen.js
@@ -1,46 +1,58 @@
 /**
  * @author Barış MERAL
  * @since 2019.10.27
- * @version 1.0.0
+ * @version 2.0.0
  */
 
 'use strict';
 
 function Fenomen() {
 
-
     const URI = "https://api.radyofenomen.com/Channels/";
     const QUERY_STRING = "/?appRef=FenomenWebV2";
     const AUDIO_URI = "https://listen.radyofenomen.com/";
-
+    let defaultQuality = 128;
 
     this.channels = {
 
-        FENOMEN: { url: URI+"radyofenomen"+QUERY_STRING, icecast: AUDIO_URI+"fenomen/256/icecast.audio" },
-        FENOMEN_TURK: { url: URI+"fenomenturk"+QUERY_STRING, icecast: AUDIO_URI+"fenomenturk/256/icecast.audio" },
-        FENOMEN_TURKCE_RAP: { url: URI+"fenomenturkcerap"+QUERY_STRING, icecast: AUDIO_URI+"fenomenturkrap/128/icecast.audio" },
-        FENOMEN_K_POP: { url: URI+"fenomenkpop"+QUERY_STRING, icecast: AUDIO_URI+"fenomenkpop/128/icecast.audio" },
-        FENOMEN_KARISIK: { url: URI+"fenomenkarisik"+QUERY_STRING, icecast: AUDIO_URI+"fenomenkarisik/128/icecast.audio" },
-        FENOMEN_POP: { url: URI+"fenomenpop"+QUERY_STRING, icecast: AUDIO_URI+"fenomenpop/128/icecast.audio" },
-        FENOMEN_DANS: { url: URI+"fenomendans"+QUERY_STRING, icecast: AUDIO_URI+"fenomendans/128/icecast.audio" }
+        FENOMEN: { name: "fenomen", url: URI + "radyofenomen" + QUERY_STRING, icecast: AUDIO_URI + "fenomen/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_TURK: { name: "fenomenturk", url: URI + "fenomenturk" + QUERY_STRING, icecast: AUDIO_URI + "fenomenturk/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_TURKCE_RAP: { name: "fenomenturkcerap", url: URI + "fenomenturkcerap" + QUERY_STRING, icecast: AUDIO_URI + "fenomenturkrap/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_K_POP: { name: "fenomenkpop", url: URI + "fenomenkpop" + QUERY_STRING, icecast: AUDIO_URI + "fenomenkpop/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_KARISIK: { name: "fenomenkarisik", url: URI + "fenomenkarisik" + QUERY_STRING, icecast: AUDIO_URI + "fenomenkarisik/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_POP: { name: "fenomenpop", url: URI + "fenomenpop" + QUERY_STRING, icecast: AUDIO_URI + "fenomenpop/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_DANS: { name: "fenomendans", url: URI + "fenomendans" + QUERY_STRING, icecast: AUDIO_URI + "fenomendans/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_CLUBBIN: { name: "fenomenclubbin", url: URI + "fenomenclubbin" + QUERY_STRING, icecast: AUDIO_URI + "fenomenclubbin/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_RAP: { name: "fenomenrap", url: URI + "fenomenrap" + QUERY_STRING, icecast: AUDIO_URI + "fenomenrap/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_AKUSTIK: { name: "fenomenakustik", url: URI + "fenomenakustik" + QUERY_STRING, icecast: AUDIO_URI + "fenomenakustik/" + defaultQuality + "/icecast.audio" },
+        FENOMEN_ORIENTAL: { name: "fenomenoriental", url: URI + "fenomenoriental" + QUERY_STRING, icecast: AUDIO_URI + "fenomenoriental/" + defaultQuality + "/icecast.audio" }
 
     }
 
+    this.get = function (callback, channel, quality = 128) {
 
-    this.get = function(channel,callback){
+        if (quality != 64 && quality != 128 && quality != 256) {
 
+            quality = 128;
+        }
 
-      fetch(channel.url).then(function (data) {
+        if ((quality == 256) && (channel != this.channels.FENOMEN || channel != this.channels.FENOMEN_TURK)) {
+
+            quality = 128;
+        }
+
+        fetch(channel.url).then(function (data) {
 
             return data.json();
-
+        
         }).then(function (json) {
 
-           let obj = {
+            let obj = {
 
                 audio: {
-                    src: channel.icecast,
-                    type: "audio/mpeg"
+                    src: AUDIO_URI + channel.name + "/" + quality + "/icecast.audio",
+                    type: "audio/mpeg",
+                    quality: quality
                 },
 
                 channel: {
@@ -59,12 +71,11 @@ function Fenomen() {
             }
 
             callback(obj);
-                    
+
         }).catch(function (e) {
             console.log(e);
         });
 
     }
-
 
 }
